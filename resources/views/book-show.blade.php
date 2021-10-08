dd4b439060cef8231d0f12a598ce80b8896a433b
deu93 <deucalion93@ukr.net>
2021-10-08 11:32:43 +0300

    1

--- resources/views/book-show.blade.php
+++ resources/views/book-show.blade.php
@@ -4,10 +4,10 @@
         <div class="flex ml-5">
             <div class="bg-white w-6/12 mb-4 pl-3 pr-2 m-2">
            
-            <h4 class="mb-2 text-lg pl-2">{{ $book->title }}</h4>
-            <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
-            <p>{{ $book->description }}</p>
-            <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-2/12">Читать с 1 главы</a>
+                <h4 class="justify-content mb-2 text-lg pl-2">{{ $book->title }}</h4>
+                <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
+                <p>{{ $book->description }}</p>
+                <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-2/12">Читать с 1 главы</a>
             
             </div> 
         
