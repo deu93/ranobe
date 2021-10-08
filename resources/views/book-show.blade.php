63915a8cc1fde1fd390de2201bcca54226614c6d
deu93 <deucalion93@ukr.net>
2021-10-08 11:20:00 +0300

    1

--- resources/views/book-show.blade.php
+++ resources/views/book-show.blade.php
@@ -2,8 +2,7 @@
 @section('content')
 <div class="w-full">
         <div class="flex ml-5">
-        @if (!$book->isEmpty())
-           <div class="bg-white w-6/12 mb-4 pl-3 pr-2 m-2">
+            <div class="bg-white w-6/12 mb-4 pl-3 pr-2 m-2">
            
             <h4 class="mb-2 text-lg pl-2">{{ $book->title }}</h4>
             <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
@@ -11,7 +10,7 @@
             <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-2/12">Читать с 1 главы</a>
             
             </div> 
-        @endif
+        
         </div>
 </div>
 
