8cdb7920085cc37807c6815ff01ad4647eebdd26
deu93 <deucalion93@ukr.net>
2021-10-08 09:55:00 +0300

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <div class="w-8/12 px-4 flex">
+    <div class="w-8/12 px-4">
         <h2>Мои ранобэ</h2>
-        @if (!$books->isEmpty())
+        <div class="flex">
+            @if (!$books->isEmpty())
             @foreach ($books as $book )
             <div class="bg-white px-2 w-3/12 mb-4">
                 <h4>{{ $book->title }}</h4>
@@ -15,9 +16,8 @@
         @else
             <h3>У вас пока нет добавленных ранобэ</h3>
         @endif
-        
-        
-        
+        </div>
+
     </div>
     <div class="flex justify-center  mt-5 mb-5">
         <a href="{{ route('add-book') }}" class="bg-blue-500 m-auto text-white px-5 no-underline  py-3 rounded font-medium w-2/12">Добавить ранобэ</a>
