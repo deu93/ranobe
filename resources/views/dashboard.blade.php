70a3b813fe124cccfbdeefa2b39f8d8276212531
deu93 <deucalion93@ukr.net>
2021-10-08 09:48:34 +0300

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             @foreach ($books as $book )
             <div class="bg-white px-2 w-3/12 mb-4">
                 <h4>{{ $book->title }}</h4>
-                <img src="{{ asset('img/' . $book->image) }}" alt="">
+                <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
                 <p>{{ $book->description }}</p>
                 <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-9/12">Редактировать</a>
                 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div class="flex ">
   @foreach ($ranobes as $ranobe)
   <div class="card pl-3 pr-2 m-2" style="width: 200px; height:550px;">
-      <img src="{{ asset('img/' . $ranobe->image) }}" class="card-img-top" alt="No image">
+      <img src="{{ asset('ranobe/public/img/' . $ranobe->image) }}" class="card-img-top" alt="No image">
       <div class="card-body">
         <h5 class="card-title m-auto">{{ $ranobe->title }}</h5>
         <p class="card-text m-auto">{{ $ranobe->description }}</p>
