e6dabdcdb0442c0d2936958699ab559471b60e66
deu93 <deucalion93@ukr.net>
2021-10-08 12:43:02 +0300

    1

--- resources/views/book-show.blade.php
+++ resources/views/book-show.blade.php
@@ -5,9 +5,9 @@
             <div class="bg-white w-12/12 mb-4 pl-3 pr-2 m-2 ml-4 ">
                <h4 class=" justify-center mb-2 text-2xl pl-2">{{ $book->title }}</h4>
                 <img class="mt-3 ml-3" src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
-                <p class="border-2">Жанры</p>
-                <p class="text-lg mb-3 mt-3 ml-3 mr-3">{{ $book->description }}</p>
-                <a type="submit" class="bg-blue-500 mt-5 text-white px-4 no-underline py-2 mb-3  rounded font-medium w-3/12">Читать с 1 главы</a>
+                <p class="border-4 p-2 m-2">Жанры</p>
+                <p class="text-lg mb-3 mt-2 mt-3 ml-3 mr-3">{{ $book->description }}</p>
+                <a type="submit" class="bg-blue-500 mt-5 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Читать с 1 главы</a>
             
             </div> 
         
