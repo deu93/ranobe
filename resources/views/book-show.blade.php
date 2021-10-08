2322fb96719c7f7d57b985513da99c5f458460b7
deu93 <deucalion93@ukr.net>
2021-10-08 11:44:34 +0300

    1

--- resources/views/book-show.blade.php
+++ resources/views/book-show.blade.php
@@ -2,12 +2,12 @@
 @section('content')
 <div class="w-full">
         <div class="flex ml-5">
-            <div class="bg-white w-12/12 mb-4 pl-3 pr-2 m-2">
+            <div class="bg-white w-12/12 mb-4 pl-3 pr-2 m-2 ml-4">
            
-                <h4 class=" justify-center mb-2 text-lg pl-2">{{ $book->title }}</h4>
+                <h4 class=" justify-center mb-2 text-2xl pl-2">{{ $book->title }}</h4>
                 <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
-                <p>{{ $book->description }}</p>
-                <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-3/12">Читать с 1 главы</a>
+                <p class="text-lg">{{ $book->description }}</p>
+                <a type="submit" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-2/12">Читать с 1 главы</a>
             
             </div> 
         
