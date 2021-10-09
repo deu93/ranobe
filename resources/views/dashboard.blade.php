5592a159166a5d505df04edfc7ab89922c45c635
Deu <deucalion93@ukr.net>
2021-10-09 16:48:54 +0300

    bug fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 @section('content')
-    <div class=" flex px-4">
+    <div class="px-4">
         <h2 class="text-lg color-white flex justify-center my-3">Мои ранобэ</h2>
         <div class="flex ml-5">
             @if (!$books->isEmpty())
             @foreach ($books as $book )
-            <div class="bg-white mb-4 pl-3 pr-2 m-2 width: 300px; height:650px;">
+            <div class="bg-white mb-4 pl-3 pr-2 m-2 width: 250px; height:550px;">
                 @php
                     $shortDescription = Str::limit($book->description, 40, '...')
                 @endphp
                 <h4 class="mb-2 text-lg pl-2">{{ $book->title }}</h4>
                 <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
                 <p>{{ $shortDescription }}</p>
-                <a href="{{ url('edit-book/' . $book->slug) }}" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-9/12">Редактировать</a>
+                <a href="{{ url('edit-book/' . $book->slug) }}" class="bg-blue-500 m-auto text-white px-4 no-underline py-2 mb-3  rounded font-medium w-3/12">Редактировать</a>
                 
             </div> 
             @endforeach 
