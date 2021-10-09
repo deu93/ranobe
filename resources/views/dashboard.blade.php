2d10e9ebaad48f0987fa2c3882f83f9b652d2e06
Deu <deucalion93@ukr.net>
2021-10-09 16:57:48 +0300

    bug fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,17 +12,23 @@
                 <h4 class="mb-2 text-lg pl-2">{{ $book->title }}</h4>
                 <img src="{{ asset('ranobe/public/img/' . $book->image) }}" alt="">
                 <p>{{ $shortDescription }}</p>
-                <div class="w-full flex justify-center">
-                    <a href="{{ url('edit-book/' . $book->slug) }}" class="bg-blue-500  text-white px-4 no-underline py-3 mb-3  rounded font-medium w-4/12">Редактировать</a>
-                    @auth()
-                     @if (auth()->user()->role > 0 && auth()->user()->id == $book->user_id )
-                        <form action="{{ url('delete-book/' . $book->slug) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="bg-red-500 mt-2 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-4/12 ">Удалить</button>
-                        </form>
-                     @endif
-                 @endauth
+                <div class="flex inline">
+                    <div class="w-full flex justify-center">
+                        <a href="{{ url('edit-book/' . $book->slug) }}" class="bg-blue-500  mt-4 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-full">Редактировать</a>
+                        
+                    </div>
+                    <div class="w-full flex justify-center">
+                        @auth()
+                         @if (auth()->user()->role > 0 && auth()->user()->id == $book->user_id )
+                            <form action="{{ url('delete-book/' . $book->slug) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="bg-red-500 mt-4 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-full ">Удалить</button>
+                            </form>
+                         @endif
+                     @endauth
+                </div>
+                    
                 </div>
                 
             </div> 
