3b1af1388d276f0ea0ea92f79a89de792187e426
deu93 <deucalion93@ukr.net>
2021-10-12 11:11:32 +0300

    bug fix

--- app/Http/Controllers/BookShowController.php
+++ app/Http/Controllers/BookShowController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use App\Models\Genre;
+use App\Models\Chapter;
 use App\Models\BookGenre;
 use Illuminate\Http\Request;
 
@@ -11,24 +12,16 @@ class BookShowController extends Controller
 {
     public function index($slug) {
         $book= Book::where('slug',$slug)->first();
-        $genres_id = BookGenre::where('book_id', $book->id)->get();
+        $chapter = Chapter::where('book_id' , $book->id )->first();
+       
         
-        $genres_array = [];
-
-        foreach($genres_id as $genre_id) {
-            array_push($genres_array, $genre_id->genres_id);
-        }
-        $genres = [];
         
-        foreach($genres_array as $genre_item) { 
-            array_push($genres, Genre::where('id', $genre_item)->first());
-        }
         $genres_menu = Genre::all();
 
         
         return view('book-show',[
             'book' => $book,
-            'genres' => $genres,
+            'chapter' => $chapter,
             'genres_menu' => $genres_menu
         ]);
     }
--- resources/views/book-show.blade.php
+++ resources/views/book-show.blade.php
@@ -22,17 +22,21 @@
                  </div>
                  
                  <p class="text-lg mb-3 mt-2 mt-3 ml-3 mr-3">{{ $book->description }}</p>
-                 <a type='submit' href="{{ url('read-chapter/' . $book->id) }}" class="bg-blue-500 mt-5 ml-3 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Читать с 1 главы</a>
-                 @auth()
-                     @if (auth()->user()->role > 0 && auth()->user()->id == $book->user_id )
-                     <a href="{{ route('add-chapter', $book->id) }}" class="bg-blue-500 mt-5 ml-5 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Добавить главы</a>
-                        <form action="{{ url('delete-book/' . $book->slug) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="bg-red-500 mt-2 text-white px-4 no-underline ml-3 py-3 mb-3  rounded font-medium w-3/12 ">Удалить</button>
-                        </form>
-                     @endif
-                 @endauth
+                 <a type='submit' href="{{ url('read-chapter/' .  $chapter->id) }}" class="bg-blue-500 mt-5 ml-3 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Читать с 1 главы</a>
+
+                 <a type='submit' href="{{ url('all-chapters/' .  $book->id) }}" class="bg-blue-500 mt-5 ml-3 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Все главы</a>
+                 <div class="flex">
+                    @auth()
+                    @if (auth()->user()->role > 0 && auth()->user()->id == $book->user_id )
+                    <a href="{{ route('add-chapter', $book->id) }}" class="bg-blue-500 mt-3 ml-3 text-white px-4 no-underline py-3 mb-3  rounded font-medium w-3/12 ">Добавить главы</a>
+                       <form action="{{ url('delete-book/' . $book->slug) }}" method="POST">
+                           @csrf
+                           @method('DELETE')
+                           <button type="submit" class="bg-red-500 text-white px-16   mt-3 ml-3 py-3 mb-3  rounded font-medium  ">  Удалить книгу </button>
+                       </form>
+                    @endif
+                @endauth
+                 </div>
              </div>
         </div>
     @else
