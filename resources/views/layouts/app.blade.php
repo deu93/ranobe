7b326ba121c7cdda5a02eafc45ae656532db2ec4
deu93 <deucalion93@ukr.net>
2021-10-29 08:07:27 +0300

    bug fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/6gxooy202pd40h3t00dy0uyvyybj2qak02mnwbhygavsayz6/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7YG7V8LL5H"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7YG7V8LL5H');
+    </script>
     <title>Ранобэ. Читать ранобэ онлайн на русском. Ранобэ онлайн!</title>
     <meta name="description" content="Ранобэ, Ранобэ онлайн! У нас всё ранобэ бесплатно и без регистрации! Читать ранобэ! ранобэ на русском языке, переводы ранобэ, популярнае ранобэ!
 "/>
